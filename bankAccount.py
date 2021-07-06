8b91200a879095ab08541852cb1fa3ca1ff9f53d
sooyeun-park <aktltsms@gmail.com>
2021-07-06 13:08:06 +0900

    210706

--- bankAccount.py
+++ bankAccount.py
@@ -1,43 +1,36 @@
 #! /usr/bin/env python
 import sys
 class Account:
-    balance = '0'
-    password = '0000'
-#    def __init__(self, passwd):
-#        self.__password = int(passwd)
-    d_account = {'soo':'0525', 'yeun':'1114'}
-    
-    def logIn(self,password):
-        if self.password != 0000 :  
-            print('please setting password')        
-        #elif self in d_account : 
-        else : 
-            sys.exit()
+    __balance = '0'
+    d_account = {}
+
+    def __init__(self, user_name, user_password):
+        self.name = f'{self}'    
+        self.__password = 'user_password'
 
     def deposit(self,amount): #입금
-        #k = input("input password :")
-        #self.logInself(k)
-        r_deposit = (int(self.balance)+ int(amount))
-        self.balance = r_deposit
-        #return self.balace 
-    
+        r_deposit = (int(self.__balance)+ int(amount))
+        self.__balance = r_deposit
+        return self.__balance
     def withdraw(self,amount): #출금
-        #self.lonIn()
-        r_withdraw = (int(self.balance)- int(amount))
-        self.balance = r_withdraw
+        r_withdraw = (int(self.__balance)- int(amount))
+        self.__balance = r_withdraw
+        return self.__balance
     
     def transfer(self, who, amount): #송금
-        #self.lonIn()
-        self.balance = int(self.balance) - int(amount)
-        who.balance = int(who.balance) + int(amount)
+        self.__balance = int(self.__balance) - int(amount)
+        who.__balance = int(who.__balance) + int(amount)
+        
         
     def showBalance(self):
-       print(self.balance, '원')
+       print(self.__balance, '원')
 
-soo = Account()
-yeun = Account()
-soo.deposit(5000)
-soo.withdraw(300)
-soo.transfer(yeun,2000)
+soo= Account('soo', '0525')
+yeun = Account('yeun','1114')
+print(soo.name)
+#print(soo.__password)
+soo.deposit(35000)
+soo.withdraw(5000)
+soo.transfer(yeun,3000)
 soo.showBalance()
 yeun.showBalance()
--- student-point_class.py
+++ student-point_class.py
@@ -2,21 +2,25 @@
 
 class Student: 
     def __init__(self, kp, ep, mp):
-        self.korean = int(kp)
-        self.english = int(ep)
-        self.math = int(mp) 
+        self.__korean = int(kp)
+        self.__english = int(ep)
+        self.__math = int(mp) 
     def showKorean(self):
-        self.korean
-        return
+        k = self.__korean
+        return k
     def showEnglish(self):
-        self.english
+        e = self.__english
+        return e
     def showMath(self):
-        self.math 
+        m = self.__math
+        return m 
     def showEverage(self):
-        (self.korean + self.english + self.math)/3
+        result = (int(self.showKorean()) + int(self.showEnglish()) + int(self.showMath()))/3
+        return result
 park = Student(100, 30, 90)
-print(park.korean, '점')
-print(park.showKorean())
-#print(park.showEnglish())
-#print(park.showMath())
-#print(park.showEverage())
+#print(park.korean(), '점')
+print(park.showKorean(),'점')
+print(park.showEnglish(),'점')
+print(park.showMath(),'점')
+print(round(park.showEverage(),2),'점')
+print(park.__korean)
